e6ccbe3003f9e57a091ae92cf4e47522d7fcd8f8
Виктор Масляков <v.maslyakov@pg.local>
2025-07-28 11:45:10 +0300

    add tasks from first module

---
+++ module_1/lesson6_step12.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+link = "http://suninjuly.github.io/registration2.html"
+
+try: 
+    
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    input1 = browser.find_element(By.XPATH, '//div/form/div[1]/div[1]/input')
+    input1.send_keys("Ivan")
+    input2 = browser.find_element(By.XPATH, '//div/form/div[1]/div[2]/input')
+    input2.send_keys("Petrov")
+    input3 = browser.find_element(By.XPATH, '//div/form/div[1]/div[3]/input')
+    input3.send_keys("IvanPetrov@mail.ru")
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+    time.sleep(1)
+
+    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+    welcome_text = welcome_text_elt.text
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
+finally:
+    time.sleep(10)
+    browser.quit()
+
